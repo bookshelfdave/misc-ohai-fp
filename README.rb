8989a78c52076cbc099552806035ca4964957bdf
Dave Parfitt <dparfitt@chef.io>
2015-03-15 10:09:19 -0400

    initial commit

---
+++ README.rb
@@ -0,0 +1,8 @@
+misc-ohai-fp
+---
+
+[ohai](https://github.com/chef/ohai) plugins to detect OCaml and Haskell (GHC).
+
+The specs don't pass in this standalone repo, but pass if copied into a cloned ohai repo.
+
+
